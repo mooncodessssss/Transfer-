dde03bcf1ab4be3968fd1a12e29d17b5cf484d32
PRIVATE-REPO4 <navnitjha49826@gmail.com>
2024-11-12 11:03:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MOON-CODES/mrcool",
+    "https://github.com/PRIVATE-REPO4/privata",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
