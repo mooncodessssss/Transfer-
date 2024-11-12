7f15fd91dd601388d3601e3bef5f3dadac8b15a3
PRIVATE-REPO4 <navnitjha49826@gmail.com>
2024-11-12 11:24:44 +0530

    Update Youtube.py

--- AviaxMusic/platforms/Youtube.py
+++ AviaxMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from AnieXEricaMusic.utils.database import is_on_off
-from AnieXEricaMusic.utils.formatters import time_to_seconds
+from AviaxMusic.utils.database import is_on_off
+from AviaxMusic.utils.formatters import time_to_seconds
 import os
 import glob
 import random
@@ -22,7 +22,7 @@ import random
 
 
 def cookie_txt_file():
-    cookie_dir = "AnieXEricaMusic/cookies"
+    cookie_dir = "AviaxMusic/cookies"
     cookies_files = [f for f in os.listdir(cookie_dir) if f.endswith(".txt")]
 
     cookie_file = os.path.join(cookie_dir, random.choice(cookies_files))
