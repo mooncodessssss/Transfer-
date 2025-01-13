fbc03bca02aa4033e4abec749a41ce8f2bb590d5
PRIVATE-REPO4 <navnitjha49826@gmail.com>
2025-01-13 19:07:38 +0530

    Update test.py

--- AviaxMusic/plugins/tools/test.py
+++ AviaxMusic/plugins/tools/test.py
@@ -1,48 +1,20 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import ChatMemberUpdated
 from ANNIEMUSIC import app
 from config import OWNER_ID
 
-# vc on
-@app.on_message(filters.video_chat_started)
-async def brah(_, msg):
-       await msg.reply("ᴠᴏɪᴄᴇ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ")
-# vc off
-@app.on_message(filters.video_chat_ended)
-async def brah2(_, msg):
-       await msg.reply("**ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ**")
+@app.on_chat_member_updated(filters.group)
+async def greet_new_member(_, member: ChatMemberUpdated):
+    # Check if the user has joined the voice chat
+    if member.new_chat_member and not member.old_chat_member:
+        if member.new_chat_member.status == "member":  # Check if they are a member
+            user = member.new_chat_member.user
+            chat_id = member.chat.id
+            
+            # Send a message mentioning the user
+            await app.send_message(
+                chat_id,
+                f"🎉 Welcome {user.mention} to the voice chat! 🎤"
+            )
 
-# invite members on vc
-@app.on_message(filters.video_chat_members_invited)
-async def brah3(app :app, message:Message):
-           text = f"♻️ ❛{message.from_user.mention}❜ 💞™🌙 ɪɴᴠɪᴛᴇᴅ "
-           x = 0
-           for user in message.video_chat_members_invited.users:
-             try:
-               text += f"🖤[{user.first_name}](tg://user?id={user.id})🖤! "
-               x += 1
-             except Exception:
-               pass
-           try:
-             await message.reply(f"{text}")
-           except:
-             pass
-
-
-@app.on_message(filters.command("math", prefixes="/"))
-def calculate_math(client, message):   
-    expression = message.text.split("/math ", 1)[1]
-    try:        
-        result = eval(expression)
-        response = f"ᴛʜᴇ ʀᴇsᴜʟᴛ ɪs : {result}"
-    except:
-        response = "ɪɴᴠᴀʟɪᴅ ᴇxᴘʀᴇssɪᴏɴ"
-    message.reply(response)
-
-###
-@app.on_message(filters.command("leavegroup")& filters.user(OWNER_ID))
-async def bot_leave(_, message):
-    chat_id = message.chat.id
-    text = f"sᴜᴄᴄᴇssғᴜʟʟʏ   ʟᴇғᴛ  !!."
-    await message.reply_text(text)
-    await app.leave_chat(chat_id=chat_id, delete=True)
+app.run()
